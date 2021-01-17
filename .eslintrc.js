a2eb13a8759159a64d0ee91ab9ee28b103999e59
antoine92190 <dupontantoine92@gmail.com>
2021-01-17 23:40:04 +0100

    (typescript) add dependencies + config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-	extends: ['plugin:vue/essential', 'eslint:recommended'],
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -11,6 +15,6 @@ module.exports = {
 		'no-unused-vars': 'warn'
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser'
   }
 }
